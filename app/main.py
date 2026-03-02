95afdd77b3b0dc880f1ccb2ec421b08cda8d88fc
Angelo <100081282+vertigotalks7@users.noreply.github.com>
2026-03-02 22:32:30 +0530

    voting system

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app import models
 from .database import ENGINE
-from .routers import post,user,auth
+from .routers import post,user,auth,vote
 from .config import settings
 
 app = FastAPI()
@@ -11,6 +11,7 @@ models.Base.metadata.create_all(bind=ENGINE)
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 ##------------- fastapi url test
 
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel,EmailStr, conint
+from pydantic import BaseModel,EmailStr,conint
 from datetime import datetime
 from typing import Optional
 
