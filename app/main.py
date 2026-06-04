2e0a066ffde4bcd5c6be80405b6e0673862590e6
Angelo <100081282+vertigotalks7@users.noreply.github.com>
2026-06-04 22:39:09 +0530

    adding requirements.txt and CORS policy in main.py

--- app/main.py
+++ app/main.py
@@ -3,12 +3,21 @@ from app import models
 from .database import ENGINE
 from .routers import post,user,auth,vote
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"]
 
 # models.Base.metadata.create_all(bind=ENGINE)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
     
-app.include_router(post.router)
+app.include_router(post.router) 
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
