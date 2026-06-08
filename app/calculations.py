dcb95d66995d841cbaf479c00fc66c793b297727
Angelo <100081282+vertigotalks7@users.noreply.github.com>
2026-06-08 19:30:41 +0530

    pytest basics

--- app/calculations.py
+++ app/calculations.py
@@ -1,2 +1,16 @@
 def add(num1: int, num2: int):
     return num1 + num2
+
+class BankAccount:
+    def __init__(self, balance=0):
+        self.balance = balance
+    
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+    
+    def withdraw(self, amount):
+        self.balance -= amount
+    
+    def collect_interest(self):
+        self.balance *= 1.1
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,4 +1,14 @@
-from app.calculations import add
-def test_add():
-    sum = add(5,3)
-    assert sum == 8
+import pytest
+from app.calculations import add,BankAccount
+@pytest.mark.parametrize("num1,num2,expected", [
+    (5, 3, 8),
+    (10, 2, 12),
+    (-1, 1, 0)
+])
+def test_add(num1, num2, expected):
+    sum = add(num1, num2)
+    assert sum == expected
+
+def test_bank_set_initial_amount():
+    account = BankAccount(100)
+    assert account.balance == 100
